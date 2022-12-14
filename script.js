501509f8d31f935136e34576d32f04a59b98db62
Luiz Serão <luiz.serao@hotmail.com>
2022-12-14 09:06:24 -0300

    Conclusão do projeto FAQ-accordion com responsivo e uso do JavaScript para exibir as respostas quando clicar na pergunta

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ dt.forEach(item => {
     item.classList.toggle("active")
   })
 })
+
+
+const img = document.querySelector(".faq-img img")
+const media = window.matchMedia("(max-width:876px)")
+window.addEventListener("resize", () => {
+  if(media.matches === true) {
+    img.setAttribute("src", "./images/illustration-woman-online-mobile.svg")
+  } else {
+    img.setAttribute("src", "./images/illustration-box-desktop.svg")
+  }
+})
